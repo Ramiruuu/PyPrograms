567f37b0e07b1a71f5874853160f9e909c3c436d
Mar <144437990+Ramiruuu@users.noreply.github.com>
2025-02-19 20:49:12 +0800

    06

---
+++ listsamples.py
@@ -0,0 +1,18 @@
+#list
+
+random = [5,6,7, "ally", "hannah"]
+
+#accessing
+print(random[4])
+
+#append
+random.append("Dodong")
+print(random)
+
+#delete
+random.remove("Dodong")
+print(random)
+
+del random[0]
+print(random)
+
